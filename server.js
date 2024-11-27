c66c3480ed0bd177b57f42540bab4242b0ac6540
eworsham <eworsham64@gmail.com>
2024-11-26 22:54:20 -0500

    Setup localhost swagger for local testing

--- server.js
+++ server.js
@@ -3,12 +3,18 @@ const app = express();
 const mongodb = require('./data/database');
 const swaggerUI = require('swagger-ui-express')
 const swaggerDocument = require('./swagger-output.json')
+const swaggerDocumentDev = require('./swagger-output-dev.json')
 
 const PORT = process.env.PORT || 3000;
 
 // Swagger Middleware
-app.use('/api-docs', swaggerUI.serve)
-app.use('/api-docs', swaggerUI.setup(swaggerDocument))
+if (process.env.DEV == 'true') {
+  app.use('/api-docs', swaggerUI.serve)
+  app.use('/api-docs', swaggerUI.setup(swaggerDocumentDev))
+} else {
+  app.use('/api-docs', swaggerUI.serve)
+  app.use('/api-docs', swaggerUI.setup(swaggerDocument))
+}
 
 // Routes
 app.use('/', require('./routes'))
--- swagger.js
+++ swagger.js
@@ -1,5 +1,21 @@
 const swaggerAutogen = require('swagger-autogen')
 
+// dev environment
+const docDev = {
+    info: {
+        title: 'Library API',
+        description: 'Library API for books and users'
+    },
+    host: 'localhost:3000',
+    schemes: ["http"]
+}
+
+const outputFileDev = './swagger-output-dev.json'
+const routesDev = ['./server.js']
+
+swaggerAutogen(outputFileDev, routesDev, docDev)
+
+// Prod environment
 const doc = {
     info: {
         title: 'Library API',
